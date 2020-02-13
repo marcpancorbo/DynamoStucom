18abd8f717d8f7ec3c7cfca3a8b5e9469b2e4363
marcpancorbo <55668102+marcpancorbo@users.noreply.github.com>
2020-02-13 17:55:52 +0100

    Test crear tabla

--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -1,11 +1,35 @@
 package view;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.model.*;
 import controller.DynamoManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     private static DynamoManager manager = DynamoManager.getInstance();
-    public static void main(String[] args) {
-        manager.testTables();
+    public static void main(String[] args) throws InterruptedException {
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
+        DynamoDB dynamoDB = new DynamoDB(client);
+        List<AttributeDefinition> attributeDefinitions= new ArrayList<AttributeDefinition>();
+        attributeDefinitions.add(new AttributeDefinition().withAttributeName("Id").withAttributeType("N"));
+
+        List<KeySchemaElement> keySchema = new ArrayList<KeySchemaElement>();
+        keySchema.add(new KeySchemaElement().withAttributeName("Id").withKeyType(KeyType.HASH));
+        CreateTableRequest request = new CreateTableRequest()
+                .withTableName("tableName") .withKeySchema(keySchema)
+                .withAttributeDefinitions(attributeDefinitions).withProvisionedThroughput(new ProvisionedThroughput()
+                        .withReadCapacityUnits(5L)
+                        .withWriteCapacityUnits(6L));
+       Table table=  dynamoDB.createTable(request);
+        table.waitForActive();
+        TableDescription tableDescription =
+                dynamoDB.getTable("tableName").describe();
+        System.out.println(tableDescription.getTableName());
     }
 
 }
