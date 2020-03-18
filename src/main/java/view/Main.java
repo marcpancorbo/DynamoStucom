6158626ff5e23a22c5b6452ae4d730b456a74d97
marcpancorbo <55668102+marcpancorbo@users.noreply.github.com>
2020-03-18 15:33:52 +0100

    Login

--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -1,17 +1,8 @@
 package view;
 
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.document.DynamoDB;
-import com.amazonaws.services.dynamodbv2.document.Table;
-import com.amazonaws.services.dynamodbv2.model.*;
 import controller.DynamoManager;
 import model.Empleado;
-import model.Incidencia;
-import model.TipoUrgencia;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public class Main {
